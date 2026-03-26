66d7ff1f16d357a71d39bc778bd4d48957c02291
chyunj2-adfs <chyunj2@gmail.com>
2026-03-26 20:44:33 +0900

    Refactor my_sum function for better readability
    Refactor my_sum function to improve output formatting and code structure.

--- code/3-5.c
+++ code/3-5.c
@@ -1,34 +1,38 @@
 #include <stdio.h>
 #include <stdarg.h>
 
-void my_sum(char type, int count, ...){
+void my_sum(char type, int count, ...)
+{
     va_list args;
     va_start(args, count);
-    int sum = 0;
-    if (type == 'S'){
+    if(type == 'S'){
         for (int i = 0; i < count; i++) {
-            char* s = va_arg(args, char*);
-            printf("%s ", s);
+            char* str = va_arg(args, char*);
+            printf("%s", str);
+            if (i < count - 1) {
+                printf(" ");
+            }
         }
         printf("\n");
-    }else if (type == 'C'){
+    }else if(type == 'C'){
         for (int i = 0; i < count; i++) {
-            char c = (char)va_arg(args, int);
-            printf("%c", c);
+            char ch = (char)va_arg(args, int);
+            printf("%c", ch);
         }
         printf("\n");
     }else if(type == 'D'){
-        for(int i = 0; i < count; i++) {
+        int sum = 0;
+        for (int i = 0; i < count; i++) {
             sum += va_arg(args, int);
         }
         printf("%d\n", sum);
     }
     va_end(args);
 }
-int main() {
+int main()
+{
     my_sum('S', 2, "Hello", "World");
     my_sum('C', 3, 'C', 'N', 'U');
     my_sum('D', 4, 10, 20, 30, 40);
-
     return 0;
 }
